4684e22819efa6dbe25f824d0c3b0b8a4350765f
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-10-30 14:26:09 +0530

    migratory birds completed

--- src/com/easy/migratoryBirds/Solution.java
+++ src/com/easy/migratoryBirds/Solution.java
@@ -14,41 +14,42 @@ import java.util.stream.Stream;
 
 public class Solution {
 
-    // Complete the migratoryBirds function below.
-    static int migratoryBirds(List<Integer> arr) {
-    	int index=0;
-    	int frequency=0;
-    	Map<Integer,Integer> map=new HashMap<>();
-    	for(int i:arr) {
-    		if(map.containsKey(i)) {
-    			map.put(i, map.get(i)+1);
-    		}else {
-    			map.put(i, 0);
-    		}
-    	}
-    	for(Map.Entry<Integer, Integer> entry :map.entrySet()) {
-    		
-    	}
-    	return index;
+	// Complete the migratoryBirds function below.
+	static int migratoryBirds(List<Integer> arr) {
+		int index = 0;
+		int frequency = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i : arr) {
+			if (map.containsKey(i)) {
+				map.put(i, map.get(i) + 1);
+			} else {
+				map.put(i, 0);
+			}
+		}
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (frequency < entry.getValue() || (frequency == entry.getValue() && index > entry.getKey())) {
+				frequency = entry.getValue();
+				index = entry.getKey();
+			}
+		}
+		return index;
+	}
 
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+		int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
 
-        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+		List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
 
-        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+		int result = migratoryBirds(arr);
 
-        int result = migratoryBirds(arr);
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
 }
